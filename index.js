021f45cbf80bff2c6310a7a51fae02592c45f0c3
Matt M <matt.d.m@hotmail.com>
2019-08-31 11:35:21 -0500

    change CORS policy to be an enviroment variable

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require('express');
-const multer  = require('multer');
+const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const imgur = require('imgur');
 const fs = require('fs');
 
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", process.env['CLIENT_DOMAIN']);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 imgur.setClientId(process.env['CLIENT_ID'])
 
 app.post('/upload', upload.single('image'), (req, res) => {
     imgur.uploadFile(req.file.path)
-    .then( (json) => {
-        res.send(json.data.link);
-    })
-    .catch( (err)  => {
-        res.send(err.message);
-    })
-    .finally(() => fs.unlinkSync(req.file.path))
+        .then((json) => {
+            res.send(json.data.link);
+        })
+        .catch((err) => {
+            res.send(err.message);
+        })
+        .finally(() => fs.unlinkSync(req.file.path))
 })
 
 app.get('/', (req, res) => {
@@ -31,6 +31,7 @@ app.get('/', (req, res) => {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+    port = 8000;
 }
+
 app.listen(port);
