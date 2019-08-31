fa2dea823294856fdc03d6d0780ab4719b8b6d0d
Matt M <matt.d.m@hotmail.com>
2019-08-31 11:43:58 -0500

    fixed merge conflict

--- index.js
+++ index.js
@@ -6,13 +6,8 @@ const fs = require('fs');
 
 const app = express();
 
-<<<<<<< HEAD
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", process.env['CLIENT_DOMAIN']);
-=======
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
->>>>>>> e5ddc95e17a31dc617ff6669f18c5ed6c85e3b52
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -38,9 +33,4 @@ let port = process.env.PORT;
 if (port == null || port == "") {
     port = 8000;
 }
-<<<<<<< HEAD
-
 app.listen(port);
-=======
-app.listen(port);
->>>>>>> e5ddc95e17a31dc617ff6669f18c5ed6c85e3b52
