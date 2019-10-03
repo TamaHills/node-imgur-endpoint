c770c56f25cdd25444c3488fe71cfb3713ae51f1
Matt M <matt.d.m@hotmail.com>
2019-10-03 13:26:13 -0500

    🆕 added logging and scripts

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const fs = require('fs');
 
 const app = express();
 
+function logger(req, res, next) {
+    console.log(`${(new Date()).toString()}: ${req.method} request -> ${req.path}`);
+    next();
+};
+
+app.use(logger);
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", process.env['CLIENT_DOMAIN']);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -14,23 +21,35 @@ app.use(function (req, res, next) {
 
 imgur.setClientId(process.env['CLIENT_ID'])
 
+app.get('/', (req, res) => {
+    res.send('imgur upload')
+})
+
 app.post('/upload', upload.single('image'), (req, res) => {
     imgur.uploadFile(req.file.path)
-        .then((json) => {
+        .then(json => {
+            console.log(`${(new Date).toString()} image published -> ${json.data.link}`)
             res.send(json.data.link);
         })
-        .catch((err) => {
+        .catch(err => {
             res.send(err.message);
         })
         .finally(() => fs.unlinkSync(req.file.path))
 })
 
-app.get('/', (req, res) => {
-    res.send('imgur upload')
-})
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 8000;
+
+const box = (inner_content, emoji_count = 0) => {
+    const spaces = inner_content.length + 4 + emoji_count
+    const line = '═'.repeat(spaces);
+    const gap = ' '.repeat(spaces);
+    return `╔${line}╗\n║${gap}║\n║  ${inner_content}  ║\n║${gap}║\n╚${line}╝`
 }
-app.listen(port);
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.clear()
+    console.log(box(`✔ the server is listening on port: ${port}`, 1))
+})
